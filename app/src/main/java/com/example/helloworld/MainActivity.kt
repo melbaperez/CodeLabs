07795f65c33fd69eb717e67dd3e4ee4122643c35
Melba Pérez Santana <91152497+melbaperez@users.noreply.github.com>
2022-10-11 21:42:32 +0100

    1.2.5.2 Task: Change the text

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -16,9 +16,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
+        val randomInt = (1..6).random()
         //Toast.makeText(this, "button clicked",
         //Toast.LENGTH_SHORT).show()
         val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "Dice Rolled!"
+        resultText.text = randomInt.toString()
     }
 }
