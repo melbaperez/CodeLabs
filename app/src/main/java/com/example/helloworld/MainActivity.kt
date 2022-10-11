b4bf68575f7f94aa0fc13d3986addd81cbde6de6
Melba Pérez Santana <91152497+melbaperez@users.noreply.github.com>
2022-10-11 22:00:52 +0100

    1.2 Coding challenge

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +12,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+        val countUpButton: Button = findViewById(R.id.countUp_button)
+        countUpButton.setOnClickListener { countUp() }
 
     }
 
@@ -21,5 +23,20 @@ class MainActivity : AppCompatActivity() {
         //Toast.LENGTH_SHORT).show()
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
+    }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        if (resultText.text.toString() == "Hello World!"){
+            resultText.text = "1"
+        }
+        else{
+            if (resultText.text.toString() != "6"){
+                var resultInt = resultText.text.toString().toInt()
+                resultInt++
+                resultText.text = resultInt.toString()
+            }
+        }
+
     }
 }
