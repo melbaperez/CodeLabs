dcf66a955c4a2884acaefabb9d554b2fac888f85
Melba Pérez Santana <91152497+melbaperez@users.noreply.github.com>
2022-10-12 17:40:20 +0100

    2.3.10.1 Task: Add a baseline constraint

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -12,7 +12,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setListeners()
     }
+
     private fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for the background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+        }
+    }
+
+    /*private fun makeColored(view: View) {
         when (view.id) {
             // Boxes using Color class colors for the background
             R.id.box_one_text -> view.setBackgroundResource(R.drawable.flor1)
@@ -22,7 +35,7 @@ class MainActivity : AppCompatActivity() {
             R.id.box_five_text -> view.setBackgroundResource(R.drawable.flor5)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
-    }
+    }*/
 
     private fun setListeners() {
 
