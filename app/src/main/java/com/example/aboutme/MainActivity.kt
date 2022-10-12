8dd4e77a43834fcd05ea4676a666cd0f2851cdfc
Melba Pérez Santana <91152497+melbaperez@users.noreply.github.com>
2022-10-12 16:26:14 +0100

    2.2.8.2 Task: Add a click listener to the nickname TextView

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -36,5 +36,10 @@ class MainActivity : AppCompatActivity() {
         editText.visibility = View.VISIBLE
         doneButton.visibility = View.VISIBLE
         view.visibility = View.GONE
+        // Set the focus to the edit text.
+        editText.requestFocus()
+        // Show the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, 0)
     }
 }
